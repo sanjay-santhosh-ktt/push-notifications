0e34cd3a2aafbf89947171981f99ff6a607dba2c
sanjay-santhosh-ktt <sanjay.santhosh@webasp.com.au>
2024-09-28 09:53:47 +0530

    new changes

--- main.js
+++ main.js
@@ -3,14 +3,7 @@ import { getMessaging, getToken } from 'https://www.gstatic.com/firebasejs/9.0.0
 
 // Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBhxlDm_wTd3qATOIrzPYroZNEFQdWni10",
-    authDomain: "test-crm-7c4a2.firebaseapp.com",
-    databaseURL: "https://test-crm-7c4a2-default-rtdb.firebaseio.com",
-    projectId: "test-crm-7c4a2",
-    storageBucket: "test-crm-7c4a2.appspot.com",
-    messagingSenderId: "858406026815",
-    appId: "1:858406026815:web:e7b6e54b894fbb94c3c28a",
-    measurementId: "G-2P7YZ36G8H"
+  //add there
 };
 
 // Initialize Firebase
